f6f4ff02e1ae85db7354e3fea180c3446cbdc610
AlexCryptoKing <108023339+AlexCryptoKing@users.noreply.github.com>
2024-11-02 09:27:33 +0100

    Update setup_alexfreqailstm.sh

--- setup_alexfreqailstm.sh
+++ setup_alexfreqailstm.sh
@@ -7,24 +7,27 @@ then
   exit
 fi
 
-# Clone the FreqAI LSTM repository
-echo "Cloning the FreqAI LSTM repository..."
-sudo git clone https://github.com/AlexCryptoKing/freqailstm.git
+# Clone the FreqAI LSTM repository if it doesn’t already exist
+if [ ! -d "freqailstm" ]; then
+  echo "Cloning the FreqAI LSTM repository..."
+  git clone https://github.com/AlexCryptoKing/freqailstm.git
+else
+  echo "Repository already cloned."
+fi
 
-# Create necessary directories in ./user_data
+# Create necessary directories in ./user_data if they don’t already exist
 echo "Creating directories in user_data..."
 mkdir -p ./user_data/models
 mkdir -p ./user_data/Notebooks
 mkdir -p ./user_data/strategies
 mkdir -p ./user_data/logs
 
+# Copy strategies and data from the cloned repo to ./user_data
+echo "Copying data from freqailstm to ./user_data/..."
 cp -r ./freqailstm/V9/* ./user_data/strategies
-
-# Copy contents from the cloned repo's user_data to the local user_data
-echo "Copying data from freqailstm/user_data to ./user_data/..."
 cp -r ./freqailstm/user_data/* ./user_data/
 
-# Change permissions of the ./user_data folder and all its subdirectories to 0777
+# Set permissions for the ./user_data folder
 echo "Changing permissions of the user_data directory to 0777..."
 chmod -R 0777 ./user_data
 
